9dfe38f29af97ef108091d93e9e5cba690edafbd
SeboCraft <simon@Simon-PC.Speedport_W723_V_Typ_A_1_01_016>
2016-08-21 16:25:30 +0200

    Maus Update

--- src/settlers/Mouse.java
+++ src/settlers/Mouse.java
@@ -1,10 +1,16 @@
 package settlers;
 
+import org.lwjgl.glfw.GLFW;
 import org.lwjgl.glfw.GLFWMouseButtonCallback;
 
 public class Mouse extends GLFWMouseButtonCallback {
 	
 	public static final int MOUSE_BUTTON_0 = 0;
+	public static final int MOUSE_BUTTON_1 = 1;
+	public static final int MOUSE_BUTTON_2 = 2;
+	public static final int MOUSE_BUTTON_3 = 3;
+	public static final int MOUSE_BUTTON_4 = 4;
+	public static final int MOUSE_BUTTON_5 = 5;
 	
 	private static boolean mouseButton_0_Down = false;
 	private static boolean mouseButton_1_Down = false;
@@ -13,10 +19,63 @@ public class Mouse extends GLFWMouseButtonCallback {
 	private static boolean mouseButton_4_Down = false;
 	private static boolean mouseButton_5_Down = false;
 	
+	public static boolean isButtonDown(int button) {
+		switch(button) {
+			case MOUSE_BUTTON_0:
+				return mouseButton_0_Down;
+			case MOUSE_BUTTON_1:
+				return mouseButton_1_Down;
+			case MOUSE_BUTTON_2:
+				return mouseButton_2_Down;
+			case MOUSE_BUTTON_3:
+				return mouseButton_3_Down;
+			case MOUSE_BUTTON_4:
+				return mouseButton_4_Down;
+			case MOUSE_BUTTON_5:
+				return mouseButton_5_Down;
+		}
+		return false;
+	}
+	
 	@Override
 	public void invoke(long window, int button, int action, int mods) {
 		switch(button) {
-			
+			case MOUSE_BUTTON_0:
+				if(action == GLFW.GLFW_PRESS)
+					mouseButton_0_Down = true;
+				else if(action == GLFW.GLFW_RELEASE)
+					mouseButton_0_Down = false;
+				break;
+			case MOUSE_BUTTON_1:
+				if(action == GLFW.GLFW_PRESS)
+					mouseButton_1_Down = true;
+				else if(action == GLFW.GLFW_RELEASE)
+					mouseButton_1_Down = false;
+				break;
+			case MOUSE_BUTTON_2:
+				if(action == GLFW.GLFW_PRESS)
+					mouseButton_2_Down = true;
+				else if(action == GLFW.GLFW_RELEASE)
+					mouseButton_2_Down = false;
+				break;
+			case MOUSE_BUTTON_3:
+				if(action == GLFW.GLFW_PRESS)
+					mouseButton_3_Down = true;
+				else if(action == GLFW.GLFW_RELEASE)
+					mouseButton_3_Down = false;
+				break;
+			case MOUSE_BUTTON_4:
+				if(action == GLFW.GLFW_PRESS)
+					mouseButton_4_Down = true;
+				else if(action == GLFW.GLFW_RELEASE)
+					mouseButton_4_Down = false;
+				break;
+			case MOUSE_BUTTON_5:
+				if(action == GLFW.GLFW_PRESS)
+					mouseButton_5_Down = true;
+				else if(action == GLFW.GLFW_RELEASE)
+					mouseButton_5_Down = false;
+				break;
 		}
 	}
 
