11dbdbdd93897483b53fdeeb3d634e57f5b50917
NewCommander97 <timotte97@gmail.com>
2016-08-21 16:32:36 +0200

    Terrain

---
+++ src/settlers/Terrain.java
@@ -0,0 +1,26 @@
+package settlers;
+
+public class Terrain {
+
+    private final GameObject[] GameObjects;
+
+    public Terrain(int blocksPerRow, float scale, float minY, float maxY, String heightMap, String textureFile, int textInc) throws Exception {
+        GameObjects = new GameObject[blocksPerRow * blocksPerRow];
+        HeightMapMesh heightMapMesh = new HeightMapMesh(minY, maxY, heightMap, textureFile, textInc);
+        for (int row = 0; row < blocksPerRow; row++) {
+            for (int col = 0; col < blocksPerRow; col++) {
+                float xDisplacement = (col - ((float) blocksPerRow - 1) / (float) 2) * scale * HeightMapMesh.getXLength();
+                float zDisplacement = (row - ((float) blocksPerRow - 1) / (float) 2) * scale * HeightMapMesh.getZLength();
+
+                GameObject terrainBlock = new GameObject(heightMapMesh.getMesh());
+                terrainBlock.setScale(scale);
+                terrainBlock.setPosition(xDisplacement, 0, zDisplacement);
+                GameObjects[row * blocksPerRow + col] = terrainBlock;
+            }
+        }
+    }
+
+    public GameObject[] getGameObjects() {
+        return GameObjects;
+    }
+}
